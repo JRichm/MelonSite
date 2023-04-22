db37beba67f8001449d9fda51b4bc3175a84446b
JRichm <jblackenmusic@gmail.com>
2023-04-22 16:05:50 -0600

    initial commit

---
+++ melons.py
@@ -0,0 +1,9 @@
+class Melon:
+    def __init__(self, melon_id, common_name, price, image_url, color, seedless):
+        self.melon_id = melon_id
+        self.common_name = common_name
+        self.price = price
+        self.image_url =image_url
+        self.color =color
+        self.seedless = seedless
+        
